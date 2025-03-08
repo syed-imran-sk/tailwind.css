5d8721f632cef669023a9d699c19a6a9577fcc74
syed-imran-sk <syedimranoffcialsk@gmail.com>
2025-03-08 19:56:21 +0530

    add nav and hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import "./App.css";
 import Nav from "./components/navbar";
+import Hero from "./pages/hero";
 
 function App() {
   return (
     <>
     <Nav/>
+    <Hero/>
+   
 
     </>
   );
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -10,10 +10,10 @@ import Logo from "../assets/mine.jpg";
 
 const navbar = () => {
   return (
-    <nav>
-      <div>
+    <nav className="flex justify-between items-center bg-amber-200">
+      <div className="h-16 w-16">
         <a>
-          <img src={Logo} alt="" />
+          <img src={Logo} alt="" className="rounded-full" />
         </a>
       </div>
       
@@ -29,13 +29,14 @@ const navbar = () => {
        */}
 
 
-       <ul>
-        <li><a href="Services.jsx">Services</a></li>
-        <li><a href="AboutUs.jsx">AboutUs</a></li>
-        <li><a href="ContactUs.jsx">ContactUs</a></li>
-        <li><a href="Pricing.jsx">Pricing</a></li>
+       <ul className="flex space-x-6"> 
+        <li><a  className="text-blue-600 hover:text-green-500 font-medium" href="index.jsx">Home</a></li>
+        <li><a  className="text-blue-600 hover:text-green-500 font-medium" href="Services.jsx">Services</a></li>
+        <li><a className="text-blue-600 hover:text-green-500 font-medium" href="AboutUs.jsx">AboutUs</a></li>
+        <li><a className="text-blue-600 hover:text-green-500 font-medium" href="ContactUs.jsx">ContactUs</a></li>
+        <li><a className="text-blue-600 hover:text-green-500 font-medium" href="Pricing.jsx">Pricing</a></li>
        </ul>
-       <button>Sign-Up</button>
+       <button className="bg-green-500 px-6 py-2 rounded-full text-white hover:bg-blue-400 font-medium">Sign-Up</button>
     </nav>
   );
 };
