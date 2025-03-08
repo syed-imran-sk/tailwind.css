c3a8dd1877350b07a5d18ee3ebc06d87a99f9fd2
syed-imran-sk <syedimranoffcialsk@gmail.com>
2025-03-08 12:28:33 +0530

    adding navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
-import { useState } from 'react'
-import './App.css'
+import "./App.css";
+import Nav from "./components/navbar";
 
 function App() {
-
   return (
     <>
-    <h1 class="text-3xl font-bold underline bg-red-400">
-    Hello world!
-  </h1>
-       
+    <Nav/>
+
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React from "react";
+import Logo from "../assets/mine.jpg";
+// import Services from "../pages/Services";
+// import AboutUs from "../pages/AboutUs"
+// import ContactUs from "../pages/ContactUs";
+// import Pricing from "../pages/Pricing";
+
+// import {BrowserRouter,Routes,Route} from "react-router-dom";
+
 
 const navbar = () => {
   return (
-    <div>navbar</div>
-  )
-}
+    <nav>
+      <div>
+        <a>
+          <img src={Logo} alt="" />
+        </a>
+      </div>
+      
+        {/* <BrowserRouter>
+          
+          <Routes>
+            <Route path="/Services" element={<Services />} />
+            <Route path="/AboutUs" element={<AboutUs />} />
+            <Route path="/ContactUs" element={<ContactUs />} />
+            <Route path="/Pricing" element={<Pricing />} />
+          </Routes>
+        </BrowserRouter>
+       */}
 
-export default navbar
+
+       <ul>
+        <li><a href="Services.jsx">Services</a></li>
+        <li><a href="AboutUs.jsx">AboutUs</a></li>
+        <li><a href="ContactUs.jsx">ContactUs</a></li>
+        <li><a href="Pricing.jsx">Pricing</a></li>
+       </ul>
+       <button>Sign-Up</button>
+    </nav>
+  );
+};
+
+export default navbar;
