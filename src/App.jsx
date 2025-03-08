557b903904ced5da57d56ca1cd39b1a3f6ce1dc9
syed-imran-sk <syedimranoffcialsk@gmail.com>
2025-03-08 11:50:06 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h1 class="text-3xl font-bold underline bg-red-400">
+    Hello world!
+  </h1>
+       
+    </>
+  )
+}
+
+export default App
